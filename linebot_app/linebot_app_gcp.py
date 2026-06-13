4db6956528c12054f6ac3d396e12d679acfbbffb
湯冠廷 <tim88688@gmail.com>
2026-06-14 01:02:28 +0800

    Add files via upload

--- linebot_app/linebot_app_gcp.py
+++ linebot_app/linebot_app_gcp.py
@@ -41,8 +41,14 @@ def linebot(request):
         body_json = json.loads(body)
         
         # 驗證並處理 webhook
+        # 驗證並處理 webhook
         handler.handle(body, signature)
         
+        # ====== 加上這段：遇到 LINE Verify 空封包直接過關 ======
+        if not body_json.get("events"):
+            return "OK"
+        # ========================================================
+
         # 提取訊息和回覆 token
         msg = body_json["events"][0]["message"]["text"]
         tk = body_json["events"][0]["replyToken"]
